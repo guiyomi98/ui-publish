91df93ad563a6f10426a3c748107c53a9bbd50af
guiyomi98 <guiyomi98@gmail.com>
2023-02-22 14:03:12 +0900

    셀렉트박스 수정

--- src/scripts/input.js
+++ src/scripts/input.js
@@ -1,15 +1,13 @@
-'use strict'
-
 const active = 'is-active'
 
 const input = {
     elem: document.body.querySelectorAll(".inputbox"),
     init: function() {
         let self = this.elem
+        // let access = (self.length > 0) ? self.forEach(e => {console.log(`페이지에 inputbox가 ${self.length}개 있습니다`)}) : console.log(`페이지에 inputbox가 없습니다`)
         let access = (self.length > 0) ? self.forEach(e => {
-            console.log(self.length)
+            
         }) : false
-        access
     },
     active: function(e) {
         const [item] = e.target.classList
